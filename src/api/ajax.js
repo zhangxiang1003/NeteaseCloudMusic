50612a892267d65a1261bcccd0f9e8d2abea24ff
张祥 <1183748553@qq.com>
2020-03-08 15:43:18 +0800

    axios

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,4 +1,31 @@
 import axios from 'axios';
+import { Toast } from "vant";
+
+axios.interceptors.request.use(
+    config => {
+        Toast.loading({
+            duration: 5000, // 持续展示 toast
+            forbidClick: true,
+            message: '加载中'
+        });
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+//响应拦截器
+axios.interceptors.response.use(
+    response => {
+        if (response.status === 200) {
+            Toast.clear();
+            return Promise.resolve(response);
+        } else {
+            Toast.fail('请求错误');
+            return Promise.reject(response);
+        }
+    }
+);
 export default function ajax(url, data = {}, type = 'GET') {
     return new Promise((resolve, reject) => {
         let promise;
